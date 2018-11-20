e7e7ba97cce5eceb1026721dac9a1419019f36b4
leoowu <leoowu@gmail.com>
2018-11-20 16:46:12 +0800

    11

---
+++ yamls/ha/start-haproxy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+MasterIP1=192.168.190.102
+MasterIP2=192.168.190.103
+MasterIP3=192.168.190.105
+MasterPort=6443
+
+docker run -d --restart=always --name HAProxy-K8S -p 6444:6444 -e MasterIP1=$MasterIP1 -e MasterIP2=$MasterIP2 -e MasterIP3=$MasterIP3 -e MasterPort=$MasterPort harbor.ucex.corp/wise2ck8s/haproxy-k8s
